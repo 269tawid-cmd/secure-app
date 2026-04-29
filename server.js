f1ff042ace6a03bc1e87022ac32a9c16a19bb5e8
269tawid-cmd <269tawid@gmail.com>
2026-04-29 11:31:22 +0600

    fixed code

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public"));
 
-// simple login API
+// login API
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
@@ -15,9 +15,19 @@ app.post("/login", (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server running");
-}); 
+const express = require("express");
+const mongoose = require("mongoose"); // ← এটা add করো
+const app = express();
+
+app.use(express.json());
+app.use(express.static("public"));
+
+// 👇 এখানেই MongoDB connect বসাও
+mongoose.connect("mongodb+srv://360tawhid_db_KING:YOUR_PASSWORD@king360.pi7ezue.mongodb.net/Madrasha")
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
