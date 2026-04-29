2b0dc53903df483722e3394a4f26b2437359320f
269tawid-cmd <269tawid@gmail.com>
2026-04-29 12:27:53 +0600

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const mongoose = require("mongoose"); // ← এটা add করো
 app.use(express.json());
 app.use(express.static("public"));
 // 👇 এখানেই MongoDB connect বসাও
-mongoose.connect("mongodb+srv://360tawhid_db_KING:YOUR_PASSWORD@king360.pi7ezue.mongodb.net/Madrasha")
 mongoose.connect("mongodb+srv://360tawhid_db_KING:kingkhan77797KING@king360.pi7ezue.mongodb.net/Madrasha")
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.log(err));
